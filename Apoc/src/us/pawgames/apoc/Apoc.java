ada1cb2bb6a3c4acbca92680a734f4060f562d3e
KENisFIS <kenisfis@gmail.com>
2015-06-06 22:04:58 -0400

    - method repAdjustForPVP() & repAdjustForMission() added to Reputation.java

--- Apoc/src/us/pawgames/apoc/Apoc.java
+++ Apoc/src/us/pawgames/apoc/Apoc.java
@@ -2,17 +2,31 @@
 
 package us.pawgames.apoc;
 
+import java.io.File;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Apoc extends JavaPlugin {
 	
+	//access the config.yml
+	File configFile = new File ("Apoc/config.yml");
+	YamlConfiguration configYaml = YamlConfiguration.loadConfiguration(configFile);
+	
+	//When the plugin is enabled...
 	@Override
 	public void onEnable() {
-		
+
+		//Let the console know that the plugin is enabled
+		getLogger().info("PAWGames Apoc plugin is Enabled");
+	
 	}
 	
+	//When the plugin is disabled...
 	@Override
 	public void onDisable() {
+		
+		//Let the console know that the plugin is disabled
+		getLogger().info("PAWGames Apoc plugin is Enabled");
 		
 	}
 	
--- Apoc/src/us/pawgames/apoc/Reputation.java
+++ Apoc/src/us/pawgames/apoc/Reputation.java
@@ -1,9 +1,37 @@
-//********************************************************************
+//*********************************************************************
 // Reputation or Rep affects game play. Depending on what a player's
 // rep is, that player will get different missions. Players can choose
-// to be 
+// to be evil, neutral, or good.
+//*********************************************************************
+
 package us.pawgames.apoc;
 
-public class Reputation {
+import org.bukkit.entity.Player;
 
+public class Reputation {
+	
+	//Get a player's current rep
+	public double getPlayerRep(Player player) {
+		
+		return 1;
+	}
+
+	//Adjust player rep for PVP combat
+	public void repAdjustForPvp() {
+		
+		//The loser of a pvp battle has no rep change, but the winner's 
+		//rep is adjusted by (3 * loserRep)
+		
+	}
+	
+	
+	//Adjust player rep for Missions
+	public void repAdjustForMission(){
+		
+		//Upon completion of a mission, the player's rep will be adjusted
+		//by the mission rep amount. If the mission is failed, then player
+		//has a negative rep adjustment.
+		
+	}
+	
 }
